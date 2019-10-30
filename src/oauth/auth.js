d484848eedf0192ec48be370de2c4452b9725fff
Shing <yow.zishing@gmail.com>
2019-10-30 16:37:04 +0700

    Fix: Update url-search-params

--- src/oauth/auth.js
+++ src/oauth/auth.js
@@ -1,60 +1,68 @@
-import {Linking} from 'react-native';
+import { Linking } from "react-native";
 
-import URLSearchParams from 'url-search-params';
+import URLSearchParams from "@ungap/url-search-params";
 
-import request from './request';
-import {query} from '../util';
+import request from "./request";
+import { query } from "../util";
 
 function getRequestToken(tokens, callbackUrl, accessType) {
-  const method = 'POST';
-  const url = 'https://api.twitter.com/oauth/request_token';
-  const body = accessType ? {x_auth_access_type: accessType} : {};
-  return request(tokens, url, {method, body}, {oauth_callback: callbackUrl})
+  const method = "POST";
+  const url = "https://api.twitter.com/oauth/request_token";
+  const body = accessType ? { x_auth_access_type: accessType } : {};
+  return request(tokens, url, { method, body }, { oauth_callback: callbackUrl })
     .then(response => response.text())
-    .then((text) => {
+    .then(text => {
       const params = new URLSearchParams(text);
       return {
-        requestToken: params.get('oauth_token'),
-        requestTokenSecret: params.get('oauth_token_secret'),
+        requestToken: params.get("oauth_token"),
+        requestTokenSecret: params.get("oauth_token_secret")
       };
     });
 }
 
 function getAccessToken(
-  {consumerKey, consumerSecret, requestToken, requestTokenSecret},
-  oauthVerifier,
+  { consumerKey, consumerSecret, requestToken, requestTokenSecret },
+  oauthVerifier
 ) {
-  const method = 'POST';
-  const url = 'https://api.twitter.com/oauth/access_token';
+  const method = "POST";
+  const url = "https://api.twitter.com/oauth/access_token";
   return request(
-    {consumerKey, consumerSecret, oauthToken: requestToken, oauthTokenSecret: requestTokenSecret},
+    {
+      consumerKey,
+      consumerSecret,
+      oauthToken: requestToken,
+      oauthTokenSecret: requestTokenSecret
+    },
     url,
-    {method},
-    {oauth_verifier: oauthVerifier},
+    { method },
+    { oauth_verifier: oauthVerifier }
   )
     .then(response => response.text())
-    .then((text) => {
+    .then(text => {
       const params = new URLSearchParams(text);
       return {
-        accessToken: params.get('oauth_token'),
-        accessTokenSecret: params.get('oauth_token_secret'),
-        id: params.get('user_id'),
-        name: params.get('screen_name'),
+        accessToken: params.get("oauth_token"),
+        accessTokenSecret: params.get("oauth_token_secret"),
+        id: params.get("user_id"),
+        name: params.get("screen_name")
       };
     });
 }
 
 const verifierDeferreds = new Map();
 
-Linking.addEventListener('url', ({url}) => {
-  const params = new URLSearchParams(url.split('?')[1]);
-  if (params.has('oauth_token') && verifierDeferreds.has(params.get('oauth_token'))) {
-    const verifierDeferred = verifierDeferreds.get(params.get('oauth_token'));
-    verifierDeferreds.delete(params.get('oauth_token'));
-    if (params.has('oauth_verifier')) {
-      verifierDeferred.resolve(params.get('oauth_verifier'));
+Linking.addEventListener("url", ({ url }) => {
+  const params = new URLSearchParams(url.split("?")[1]);
+  if (
+    params.has("oauth_token") &&
+    verifierDeferreds.has(params.get("oauth_token"))
+  ) {
+    const verifierDeferred = verifierDeferreds.get(params.get("oauth_token"));
+    verifierDeferreds.delete(params.get("oauth_token"));
+    if (params.has("oauth_verifier")) {
+      verifierDeferred.resolve(params.get("oauth_verifier"));
     } else {
-      verifierDeferred.reject(new Error('denied'));
+      verifierDeferred.reject(new Error("denied"));
     }
   }
 });
@@ -62,23 +70,29 @@ Linking.addEventListener('url', ({url}) => {
 export default async function auth(
   tokens,
   callbackUrl,
-  {accessType, forSignIn = false, forceLogin = false, screenName = ''} = {},
+  { accessType, forSignIn = false, forceLogin = false, screenName = "" } = {}
 ) {
-  const usePin = typeof callbackUrl.then === 'function';
-  const {requestToken, requestTokenSecret} = await getRequestToken(
+  const usePin = typeof callbackUrl.then === "function";
+  const { requestToken, requestTokenSecret } = await getRequestToken(
     tokens,
-    usePin ? 'oob' : callbackUrl,
-    accessType,
+    usePin ? "oob" : callbackUrl,
+    accessType
   );
-  Linking.openURL(`https://api.twitter.com/oauth/${forSignIn ? 'authenticate' : 'authorize'}?${
-    query({oauth_token: requestToken, force_login: forceLogin, screen_name: screenName})
-  }`);
+  Linking.openURL(
+    `https://api.twitter.com/oauth/${
+      forSignIn ? "authenticate" : "authorize"
+    }?${query({
+      oauth_token: requestToken,
+      force_login: forceLogin,
+      screen_name: screenName
+    })}`
+  );
   return getAccessToken(
-    {...tokens, requestToken, requestTokenSecret},
-    await (
-      usePin ?
-        callbackUrl :
-        new Promise((resolve, reject) => {verifierDeferreds.set(requestToken, {resolve, reject});})
-    ),
+    { ...tokens, requestToken, requestTokenSecret },
+    await (usePin
+      ? callbackUrl
+      : new Promise((resolve, reject) => {
+          verifierDeferreds.set(requestToken, { resolve, reject });
+        }))
   );
 }
